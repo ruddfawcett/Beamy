8c1850cbc8c06ac0c117a27a83c37ce346905232
Rudd Fawcett <rudd.fawcett@gmail.com>
2018-01-26 14:26:55 -0500

    Added empty project

---
+++ Beamy/Beamy.h
@@ -0,0 +1,19 @@
+//
+//  Beamy.h
+//  Beamy
+//
+//  Created by Rudd Fawcett on 1/26/18.
+//  Copyright © 2018 Beamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Beamy.
+FOUNDATION_EXPORT double BeamyVersionNumber;
+
+//! Project version string for Beamy.
+FOUNDATION_EXPORT const unsigned char BeamyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Beamy/PublicHeader.h>
+
+
---
+++ Beamy/Test.swift
@@ -0,0 +1,10 @@
+//
+//  Test.swift
+//  Beamy_iOS
+//
+//  Created by Rudd Fawcett on 1/26/18.
+//  Copyright © 2018 Beamy. All rights reserved.
+//
+
+import Foundation
+
