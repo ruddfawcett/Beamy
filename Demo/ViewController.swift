a8f812974ee9b3c1b1e71eb93ca17430ababf4e8
Rudd Fawcett <rudd.fawcett@gmail.com>
2018-01-28 14:38:59 -0500

    Added demo project

---
+++ Demo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Rudd Fawcett on 1/30/18.
+//  Copyright © 2018 Beamy. All rights reserved.
+//
+
+import UIKit
+import Beamy_iOS
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
