30c31fb7191bdc98a59c3b84db9d758080a8ad5b
Rudd Fawcett <rudd.fawcett@gmail.com>
2018-01-30 14:39:17 -0500

    Added Beamy manager

---
+++ Beamy/Beamy.swift
@@ -0,0 +1,58 @@
+//
+//  Beamy.swift
+//  Beamy
+//
+//  Created by Rudd Fawcett on 1/26/18.
+//  Copyright © 2018 Beamy. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+enum BeamyState {
+    
+}
+
+public class BeamyScanner: NSObject  {
+    let sharedInstance = BeamyScanner()
+    var peripherals: [CBPeripheral] = []
+    var centralManager: CBCentralManager!
+//    let scanner: Scanner?
+    
+    let identifier: String = Bundle.main.bundleIdentifier!
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
+    }
+}
+
+extension BeamyScanner: CBCentralManagerDelegate {
+    public func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            self.centralManager.scanForPeripherals(withServices: nil, options: nil)
+        }
+        else {
+            // Bluetooth may be off.
+        }
+    }
+    
+    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !peripherals.contains(peripheral) {
+            peripherals.append(peripheral)
+            print(peripheral.name ?? "No peripheral name...")
+        }
+    }
+
+    public func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+
+    }
+
+    public func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+
+    }
+
+    public func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+
+    }
+}
