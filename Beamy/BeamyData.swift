d8b492472d4c931cae5ceb116083e43daa52e30a
Rudd Fawcett <rudd.fawcett@gmail.com>
2018-02-08 15:14:54 -0500

    Rename AdvertisementData to Data

---
+++ Beamy/BeamyData.swift
@@ -0,0 +1,14 @@
+//
+//  BeamyData.swift
+//  Beamy_iOS
+//
+//  Created by Rudd Fawcett on 2/2/18.
+//  Copyright © 2018 Beamy. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+struct BeamyData {
+
+}
