aad18daaafa869000e7629195476f5055232290a
Rudd Fawcett <rudd.fawcett@gmail.com>
2018-02-11 16:42:19 -0500

    Bug fix where message was always nil

--- Beamy/BeamyMessage.swift
+++ Beamy/BeamyMessage.swift
@@ -7,41 +7,69 @@
 //
 
 import Foundation
+import CoreBluetooth
 
 enum BeamyMessageType {
     case string, data, vibration
 }
 
-struct BeamyMessage<T> {
-    var body: T!
+struct BeamyMessage {
+    // From broadcasted device...
+    let rawData: [String : Any]
+    var data: [String: Any] = [:]
+    var messageID: String?
+    
+    // From client device...
+    var body: Any!
     var dataString: String {
         get {
-            return self.toDataString()
+            return self.generate() ?? ""
         }
     }
-    
-    init(_  body: T) {
+
+    init(_  body: Any) {
         self.body = body
+        self.rawData = [:]
     }
     
-    func toDataString() -> String {
-        var bodyString = ""
+    init(data: [String : Any]) {
+        self.rawData = data
         
-        if self.body is String {
-            bodyString = self.body as! String
+        if let dataString = data[CBAdvertisementDataLocalNameKey] as? String {
+            self.parse(dataString: dataString)
+        }
+    }
+    
+    private mutating func parse(dataString string: String) {
+        do {
+            if let decodedData = string.base64Decoded()?.data(using: .utf8) {
+                let decodedDictionary = try JSONSerialization.jsonObject(with: decodedData, options: [])
+                if let dictionary = decodedDictionary as? [String:String] {
+                    self.body = dictionary["body"]
+                }
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func generate() -> String! {
+        var payload:[String: String] = [:]
+        
+        if let value = self.body as? String {
+            payload["body"] = value
+        }
+        else if let value = self.body as? Data {
+            payload["body"] = value.base64EncodedString(options: .init(rawValue: 0))
         }
         
-        if self.body is Data {
-            let data = self.body as! Data
-            bodyString = data.base64EncodedString(options: .init(rawValue: 0))
+        do {
+            let json = try JSONSerialization.data(withJSONObject: payload, options: [])
+            let string = String(data: json, encoding: .utf8)
+            return string!.base64Encoded()!
+        } catch {
+            print(error.localizedDescription)
+            return ""
         }
-        
-        let payload: [String: String] = [
-            "body": ,
-            "type":
-        ]
-        
-//        return
-        return ""
     }
 }
